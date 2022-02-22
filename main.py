37f4e752a568513d33430a42141089b501171ebd
CIPRE Alban <CIPRE@bts.leo>
2022-02-22 17:44:25 +0100

    first fonctional version

--- main.py
+++ main.py
@@ -5,26 +5,30 @@ from pathlib import Path
 import settings
 import sys
 import os
+import time
 
 class vboxToProxmox():
 
-    def __init__(self, ovaPath, pveIP, pvePort, pveLogin, pvePass):
+    def __init__(self, ovaPath, pveIP, pvePort, pveLogin, pvePass, vmId, storage):
         #self.vboxPath = "C:\\Program Files\\Oracle\VirtualBox"
         self.ovaPath = Path(ovaPath)
         self.ovaName = os.path.basename(self.ovaPath)
         self.pveIp = pveIP
         self.pveLogin = pveLogin
         self.pvePass = pvePass
+        self.vmId = vmId
+        self.storageDisk = storage
 
         self.run()
 
     def run(self):
         self.connection()
         #self.sendOva()
-        self.unzipOva()
+        #self.unzipOva()
+        self.createVM()
 
     def connection(self):
-        print("[*] Connecting to host")
+        print("[$] Connecting to host")
         self.ssh = SSHClient()
         self.ssh.load_system_host_keys()
         self.ssh.connect(self.pveIp, 22, self.pveLogin, self.pvePass, timeout=20)
@@ -34,20 +38,33 @@ class vboxToProxmox():
         def progress(filename, size, sent):
             sys.stdout.write("%s's progress: %.2f%%   \r" % (filename, float(sent)/float(size)*100) )
        
-        print("[*] Uploading OVA")
+        print("[$] Uploading OVA")
         scp = SCPClient(self.ssh.get_transport(), progress=progress)
-        scp.put(self.ovaPath)
-        print("\n [*] OVA uploaded")
+        self.ssh.exec_command("mkdir ovascript")
+        scp.put(self.ovaPath, "./ovascript/{}".format(self.ovaName))
+        print("\n [$] OVA uploaded")
 
         
     def unzipOva(self):
-        print("[*] Unzipping the ova")
-        command = "mkdir ovascript && mv {} ./ovascript && cd ovascript && tar -xvf {}".format(self.ovaName)
+        print("[$] Unzipping the ova")
+        command = "cd ./ovascript && tar -xvf {}".format(self.ovaName)
         stdin, stdout, stderr = self.ssh.exec_command(command)
-        print("[*] OVA Unzipped")
-        print(stderr)
+        print("[$] OVA Unzipped")
+        print(stdout.readlines())
 
+    def createVM(self):
+        print("[$] Creating the VM")
+        importOvfCommand = "cd ~/ovascript && qm importovf {} *.ovf {}".format(self.vmId, self.storageDisk)
+        importDiskCommand = "cd ~/ovascript && qm importdisk {} *.vmdk {}".format(self.vmId, self.storageDisk)
 
+        print("[$] Importing ovf file")
+        stdin, stdout, stderr = self.ssh.exec_command(importOvfCommand)
+        time.sleep(5)
+        print("[$] Importing VM disk")
+        stdin, stdout, stderr = self.ssh.exec_command(importDiskCommand, get_pty=True)
+
+        for line in iter(stdout.readline, ""):
+            print(line, end="")
 
 if __name__ == "__main__":        
     vboxToProxmox(
@@ -56,4 +73,6 @@ if __name__ == "__main__":
         settings.sshPort,
         settings.loginProxmox,
         settings.passProxmox,
+        settings.vmID,
+        settings.storage,
         )
--- settings.py
+++ settings.py
@@ -3,3 +3,5 @@ ipProxmox = "192.168.5.164" # Adresse ip de l'hyperviseur proxmox
 loginProxmox = "root" # Nom de compte de l'utilisateur proxmox (root par defaut)
 passProxmox = "mdpgr4" # Mot de passe utilisateur proxmox
 sshPort = 22 # Port SSH (22 par defaut)
+vmID = 139 # l'ID désiré pour la VM sur proxmox 
+storage = "HDD-1" # Le disque ou sera stocker la VM
