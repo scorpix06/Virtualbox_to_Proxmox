255eba32f52cda9541f27ea8c5dc6bd6e72b8f70
scorpix06 <62959825+scorpix06@users.noreply.github.com>
2022-02-23 11:34:50 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-ovaPath = r"C:\Users\cipre\Documents\Alban.ova"
-ipProxmox = "192.168.5.164" # Adresse ip de l'hyperviseur proxmox
-loginProxmox = "root" # Nom de compte de l'utilisateur proxmox (root par defaut)
-sshPort = 22 # Port SSH (22 par defaut)
+ovaPath = r"C:\Users\admin\Documents\myvm.ova"
+ipProxmox = "192.168.1.164" # IP of the Proxmox Hypervisor
+loginProxmox = "root" # Username of Proxmox server account
+sshPort = 22 # SSH port (default is 22)
 diskStorage = "HDD-1" # Disque sur lequel installé la VM sur le serveur proxmox
